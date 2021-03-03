ce76f73dffa250fe6d0acc251d08cc3a37be22a6
Hektor Misplon <Hektor Misplon>
2021-03-03 15:36:58 +0100

    Set terrain to static

--- src/components/terrain.component.svelte
+++ src/components/terrain.component.svelte
@@ -5,6 +5,7 @@
 
   const loader = new GLTFLoader().setPath("/");
   loader.load("terrain.glb", function (gltf) {
+    gltf.scene.matrixAutoUpdate = false;
     scene.add(gltf.scene);
   });
 </script>
