c61bc428172cd5bff729e7abe80809b0a014720a
Hektor Misplon <hektor.misplon@pm.me>
2021-03-17 20:27:07 +0100

    Add MIDI events to MIDI page

--- src/pages/midi.page.svelte
+++ src/pages/midi.page.svelte
@@ -1,5 +1,7 @@
 <script>
   import MIDIDevices from "../components/midi-devices.component.svelte";
+  import MIDIEvents from "../components/midi-events.component.svelte";
 </script>
 
 <MIDIDevices />
+<MIDIEvents />
