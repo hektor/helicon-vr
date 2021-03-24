aba7138505ecc046e3d3c6e23b08f712e433a6e5
Hektor Misplon <hektor.misplon@pm.me>
2021-03-25 00:52:37 +0100

    Add basic layout

--- src/pages/requirements.page.svelte
+++ src/pages/requirements.page.svelte
@@ -1,37 +1,53 @@
 <div class="container">
   <h1>Requirements</h1>
-  <section>
-    <h2>Browser support</h2>
-    Recommended
-    <ul>
-      <li>Chrome</li>
-      <li>Chromium</li>
-      <li>Brave</li>
-    </ul>
-    Other
-    <ul>
-      <li>Edge</li>
-      <li>Opera</li>
-    </ul>
-    <hr />
-    <div class="links">
-      <a href="https://caniuse.com/?search=midi"> MIDI support</a>
-      <a href="https://caniuse.com/?search=webxr"> WebXR support</a>
-    </div>
-  </section>
-  <section>
-    <h2>Hardware requirements</h2>
-    <ul>
-      <li>8GB+ RAM</li>
-      <li>Recent CPU</li>
-      <li>Recent graphics card</li>
-    </ul>
-  </section>
+  <hr />
+  <div class="row">
+    <section>
+      <h2>Browser support</h2>
+      <ul>
+        <li>Chrome (recommended)</li>
+        <li>Chromium (recommended)</li>
+        <li>Brave (recommended)</li>
+      </ul>
+      <ul>
+        <li>Edge (not recommended)</li>
+        <li>Opera (not recommended)</li>
+      </ul>
+      <hr />
+      <div class="links">
+        <a href="https://caniuse.com/?search=midi"> MIDI support</a>
+        <a href="https://caniuse.com/?search=webxr"> WebXR support</a>
+      </div>
+    </section>
+    <section>
+      <h2>Hardware requirements</h2>
+      <ul>
+        <li>8GB+ RAM</li>
+        <li>Recent CPU</li>
+        <li>Recent graphics card</li>
+      </ul>
+    </section>
+  </div>
 </div>
 
 <style>
   .container {
     flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 3.2rem;
+  }
+
+  .row {
+    flex: 1;
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  section {
+    flex: 1;
+    margin: 1.6rem 6.4rem 0 0;
+    min-width: 32rem;
   }
 
   .links {
@@ -46,7 +62,7 @@
 
   li {
     list-style: none;
-    padding: 0.8rem 0;
-    border-bottom: 1px solid #ccc;
+    padding: 1.6rem 0;
+    border-bottom: 1px solid var(--color-1);
   }
 </style>
