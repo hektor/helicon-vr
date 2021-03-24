541fef6f9c5db6bed23c92c25f97ab6a08938c16
Hektor Misplon <hektor.misplon@pm.me>
2021-03-24 18:37:01 +0100

    Create 'transport controls'

---
+++ src/components/transport-controls.svelte
@@ -0,0 +1,72 @@
+<script>
+  import Play32 from 'carbon-icons-svelte/lib/Play32'
+  import Pause32 from 'carbon-icons-svelte/lib/Pause32'
+  import Stop32 from 'carbon-icons-svelte/lib/Stop32'
+  import { playing$, bpm$ } from '../stores/playback'
+
+  /*
+   * Render transport controls and handle state updates
+   */
+</script>
+
+<div class="transport-controls">
+  <div class="field tooltip key">
+    <label for="bpm">BPM</label>
+    <input type="number" bind:value={$bpm$} step="0.1" min="1" max="10000" />
+  </div>
+  <button on:click={() => playing$.next(!$playing$)} class="tooltip key" class:playing={$playing$}>
+    {#if $playing$}
+      <Pause32 />
+      <small>Pauze</small>
+    {:else}
+      <Play32 />
+      <small>Play</small>
+    {/if}
+  </button>
+  <button class="tooltip key" disabled={!$playing$} on:click={() => playing$.next(false)}>
+    <Stop32 />
+    <small>Stop</small>
+  </button>
+</div>
+
+<style>
+  button {
+    display: flex;
+    align-items: center;
+    padding: 0.8rem 0.8rem;
+    min-height: 6.4rem;
+    min-width: 9.6rem;
+  }
+
+  .field {
+    display: flex;
+    align-items: center;
+    min-height: 6.4rem;
+    max-width: 12.8rem;
+  }
+
+  .field > input {
+    background: transparent;
+    min-height: 6.4rem;
+    border: none;
+    color: var(--color-primary);
+  }
+
+  .field > label {
+    padding: 1.6rem;
+  }
+
+  .transport-controls {
+    flex: 1;
+    display: flex;
+    border-bottom: 1px solid #ccc;
+  }
+
+  button:disabled.playing {
+    color: var(--playing);
+  }
+
+  button:disabled {
+    color: var(--color-5);
+  }
+</style>
