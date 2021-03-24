e1ab568f7a5171f921965af95ec0878449959a83
Hektor Misplon <hektor.misplon@pm.me>
2021-03-24 18:36:43 +0100

    Make RxJS compatible w/ Svelte's 'bind'

--- src/stores/playback.js
+++ src/stores/playback.js
@@ -5,8 +5,17 @@ export const bpm$ = new BehaviorSubject(localStorage.getItem('bpm') || 120)
 export const vol$ = new BehaviorSubject(localStorage.getItem('vol') || 100)
 
 /*
+ * Support "binding"
+ */
+bpm$.set = bpm$.next
+
+/*
  * Persist playback settings
  */
 
 vol$.subscribe(vol => localStorage.setItem('vol', vol))
 bpm$.subscribe(bpm => localStorage.setItem('bpm', bpm))
+
+playing$.subscribe(console.log)
+bpm$.subscribe(console.log)
+vol$.subscribe(console.log)
