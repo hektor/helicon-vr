b8605548b88418306a1ccff77f3aef46a185d4f7
Hektor Misplon <hektor.misplon@pm.me>
2021-03-26 20:08:39 +0100

    Add channel strips

---
+++ src/components/channel-strip.component.svelte
@@ -0,0 +1,49 @@
+<script>
+  import { createEventDispatcher } from 'svelte'
+  const dispatch = createEventDispatcher()
+
+  import VolumeSlider from '../components/volume-slider.component.svelte'
+  export let label
+  export let volume
+  export let mute
+  export let master
+</script>
+
+<div class="channel-strip" class:master>
+  <VolumeSlider {label} {mute} bind:value={volume} />
+  <input type="checkbox" class="channel-mute" class:mute bind:checked={mute} />
+  <button
+    class="channel-mute"
+    class:mute
+    on:click={() => {
+      mute = !mute
+    }}>M</button
+  >
+</div>
+
+<style>
+  .channel-strip {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-width: 9.6rem;
+  }
+
+  .channel-mute {
+    margin: 0.8rem;
+    padding: 0.8rem;
+    color: var(--color-3);
+    border: 1px solid var(--color-1);
+    background: var(--color-bg);
+  }
+
+  input[type='checkbox'] {
+    display: none;
+  }
+
+  .mute {
+    color: var(--color-bg);
+    border: 1px solid var(--color-bg);
+    background: var(--color-primary);
+  }
+</style>
--- src/pages/test.page.svelte
+++ src/pages/test.page.svelte
@@ -1,12 +1,20 @@
 <script>
   import TransportControls from '../components/transport-controls.svelte'
-  import VolumeSlider from '../components/volume-slider.component.svelte'
   import ToggleTheme from '../components/toggle-theme.component.svelte'
+  import ChannelStrip from '../components/channel-strip.component.svelte'
+  import AddFilled24 from 'carbon-icons-svelte/lib/AddFilled24'
   import { onDestroy } from 'svelte'
   import * as Tone from 'tone'
   import { Transport } from 'tone'
   import { playing$, bpm$ } from '../stores/playback'
   import { vol$, mute$ } from '../stores/playback'
+  import { tracks$ } from '../stores/mixer'
+
+  const newTrack = () =>
+    tracks$.next([
+      ...$tracks$,
+      { id: $tracks$.length + 1, label: `Track ${$tracks$.length + 1}`, volume: -60, mute: false },
+    ])
 
   const master = new Tone.Channel({
     volume: -Infinity,
@@ -79,16 +87,22 @@
     <option value="triangle">Triangle</option>
   </select>
   <div class="channel-strips">
-    <div class="channel-strip">
-      <VolumeSlider label="Track 1" />
-      <button class="channel-mute">M</button>
+    <div class="tracks">
+      {#each $tracks$ as { label, volume, mute }}
+        <ChannelStrip {label} bind:volume bind:mute />
+      {/each}
     </div>
-    <div class="channel-strip master">
-      <VolumeSlider label="Master" bind:value={$vol$} mute={$mute$} />
-      <button class="channel-mute" class:mute={$mute$} on:click={() => mute$.next(!$mute$)}>
-        M
-      </button>
-    </div>
+    <button class="channel-strip-add" on:click={newTrack}>
+      <AddFilled24 style="fill: var(--color-2)" />
+      <small> New track </small>
+    </button>
+    <ChannelStrip
+      label="Master"
+      mute={$mute$}
+      bind:volume={$vol$}
+      on:mute={() => mute$.next(!$mute$)}
+      master
+    />
   </div>
 </div>
 
@@ -97,35 +111,55 @@
     flex: 1;
     display: flex;
     flex-direction: column;
-  }
-
-  .channel-strip {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+    overflow: hidden;
   }
 
   .channel-strips {
+    min-width: 9.6rem;
+    max-width: 100vw;
     margin-top: auto;
     display: flex;
-    padding: 1.6rem 6.4rem;
+    padding: 1.6rem 1.6rem;
+    padding-right: 4rem;
   }
 
-  .master {
+  .channel-strip-add {
+    min-width: 9.6rem;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 1.6rem;
+    border-left: 1px solid var(--color-1);
+  }
+
+  .tracks::-webkit-scrollbar {
+    width: 5px;
+    height: 0.8rem;
+    background-color: transparent;
+    border-bottom: 1px solid var(--color-1);
+  }
+
+  .tracks::-webkit-scrollbar-thumb {
+    background: var(--color-3);
+  }
+
+  .tracks {
+    flex: 8;
+    display: flex;
+    overflow-x: scroll;
+  }
+
+  .channel-strip-add > small {
+    padding: 0.8rem 0;
+    color: var(--color-2);
+  }
+
+  :global(.master) {
+    flex: 1;
     margin-left: auto;
-  }
-
-  .channel-mute {
-    margin: 1rem;
-    padding: 1rem;
-    color: var(--color-3);
-    border: 1px solid var(--color-1);
-    background: var(--color-bg);
-  }
-
-  .mute {
-    color: var(--color-bg);
-    border: 1px solid var(--color-bg);
-    background: var(--color-primary);
+    padding-left: 3.2rem;
+    border-left: 1px solid var(--color-1);
   }
 </style>
