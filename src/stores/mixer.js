4c887e5b5f3de29ba3a6d03f2fba81540c957794
Hektor Misplon <hektor.misplon@pm.me>
2021-03-28 22:09:49 +0200

    Use sensible defaults for faders

--- src/stores/mixer.js
+++ src/stores/mixer.js
@@ -6,11 +6,10 @@ export const master$ = writable$({ label: 'Master', volume: -60, mute: false })
 
 export const tracks$ = writable$(
   JSON.parse(localStorage.getItem('tracks')) || [
-    { id: 1, label: 'Track 1', volume: -60, mute: false },
-    { id: 2, label: 'Track 2', volume: -60, mute: false },
-    { id: 3, label: 'Track 3', volume: -60, mute: false },
-    { id: 4, label: 'Track 4', volume: -60, mute: false },
-    { id: 5, label: 'Track 5', volume: -60, mute: false },
+    { id: 1, label: 'Track 1', volume: 0, mute: false },
+    { id: 2, label: 'Track 2', volume: 0, mute: false },
+    { id: 3, label: 'Track 3', volume: 0, mute: false },
+    { id: 4, label: 'Track 4', volume: 0, mute: false },
   ],
 )
 
