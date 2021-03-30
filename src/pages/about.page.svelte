f03abeff576f0336f3a9d648e6c9a4eec4def450
Hektor Misplon <hektor.misplon@pm.me>
2021-03-30 12:08:07 +0200

    Add about page header

--- src/pages/about.page.svelte
+++ src/pages/about.page.svelte
@@ -1,1 +1,19 @@
-<h1>About</h1>
+<script>
+  import Header from '../components/header.component.svelte'
+  import NavBack from '../components/nav-back.component.svelte'
+</script>
+
+<div class="container">
+  <Header>
+    <NavBack />
+  </Header>
+  <h1>About</h1>
+</div>
+
+<style>
+  .container {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
