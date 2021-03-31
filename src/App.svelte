f48f7f21b173b9656021bd0c4bbf7e177229eb64
Hektor Misplon <hektor.misplon@pm.me>
2021-03-31 22:59:13 +0200

    Centralize global styling

--- src/App.svelte
+++ src/App.svelte
@@ -21,30 +21,3 @@
 </script>
 
 <Router {routes} />
-
-<style>
-  :global(:root) {
-    --tp-base-background-color: hsla(0, 0%, 7%, 1);
-    --tp-base-shadow-color: hsla(0, 0%, 5%, 1);
-    --tp-button-background-color: hsla(0, 0%, 20%, 1);
-    --tp-button-background-color-active: rgba(89, 89, 89, 1);
-    --tp-button-background-color-focus: rgba(77, 77, 77, 1);
-    --tp-button-background-color-hover: rgba(64, 64, 64, 1);
-    --tp-button-foreground-color: hsla(0, 0%, 80%, 1);
-    --tp-folder-background-color: hsla(0, 0%, 13%, 1);
-    --tp-folder-background-color-active: hsla(0, 0%, 28%, 1);
-    --tp-folder-background-color-focus: hsla(0, 0%, 23%, 1);
-    --tp-folder-background-color-hover: hsla(0, 0%, 18%, 1);
-    --tp-folder-foreground-color: hsla(0, 0%, 80%, 1);
-    --tp-input-background-color: hsla(0, 0%, 11%, 1);
-    --tp-input-background-color-active: hsla(0, 0%, 26%, 1);
-    --tp-input-background-color-focus: hsla(0, 0%, 21%, 1);
-    --tp-input-background-color-hover: hsla(0, 0%, 16%, 1);
-    --tp-input-foreground-color: hsla(0, 0%, 80%, 1);
-    --tp-input-guide-color: hsla(0, 0%, 20%, 1);
-    --tp-monitor-background-color: hsla(0, 0%, 7%, 1);
-    --tp-monitor-foreground-color: hsla(0, 0%, 80%, 1);
-    --tp-label-foreground-color: hsla(0, 0%, 80%, 1);
-    --tp-separator-color: hsla(0, 0%, 7%, 1);
-  }
-</style>
