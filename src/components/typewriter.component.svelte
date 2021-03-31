99a599b91f9bd5c55336abb0ed6af91de28fdacd
Hektor Misplon <hektor.misplon@pm.me>
2021-03-31 15:48:30 +0200

    Add typewriter component

---
+++ src/components/typewriter.component.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let text = ''
+  export let duration = 3000
+  const { length } = text
+</script>
+
+<span style="--char-width: {length}ch; --steps: {length}; --duration: {`${duration / 1000}s`}">
+  {text}
+</span>
+
+<style>
+  span {
+    font-size: 2.4rem;
+    width: var(--char-width);
+    animation: typing var(--duration) steps(var(--steps)), blink 0.5s step-end infinite alternate;
+    white-space: nowrap;
+    overflow: hidden;
+    font-family: monospace;
+  }
+
+  @keyframes typing {
+    from {
+      width: 0;
+    }
+  }
+
+  @keyframes blink {
+    50% {
+      border-color: transparent;
+    }
+  }
+</style>
