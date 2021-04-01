42a89150d18e58b4a8934749935ec19f61abb2b9
Hektor Misplon <hektor.misplon@pm.me>
2021-04-01 23:10:40 +0200

    Add 'envelope-settings' component

---
+++ src/components/envelope-settings.component.svelte
@@ -0,0 +1,71 @@
+<script>
+  import { synth$ } from '../stores/synths'
+  import ControlKnob from './control-knob.component.svelte'
+</script>
+
+<div class="envelope">
+  <h2>Envelope</h2>
+  <div class="action-group">
+    <fieldset>
+      <ControlKnob id="attack" min={0} max={2} step={0.02} bind:value={$synth$.envelope.attack} />
+      <label for="attack">Attack</label>
+    </fieldset>
+    <fieldset>
+      <ControlKnob id="decay" min={0} max={2} step={0.02} bind:value={$synth$.envelope.decay} />
+      <label for="decay">Decay</label>
+    </fieldset>
+    <fieldset>
+      <ControlKnob id="sustain" min={0} max={1} step={0.01} bind:value={$synth$.envelope.sustain} />
+      <label for="sustain">Sustain</label>
+    </fieldset>
+    <fieldset>
+      <ControlKnob id="release" min={0} max={5} step={0.05} bind:value={$synth$.envelope.release} />
+      <label for="release">Release</label>
+    </fieldset>
+    <hr />
+    <fieldset>
+      <ControlKnob id="portamento" min={0} max={5} step={0.05} bind:value={$synth$.portamento} />
+      <label for="portamento">Portamento</label>
+    </fieldset>
+  </div>
+</div>
+
+<style>
+  hr {
+    border-color: var(--color-1);
+  }
+
+  .envelope {
+    padding: 1.6rem 0.8rem;
+    border: 1px solid var(--color-1);
+    margin-top: -1px;
+    margin-left: -1px;
+  }
+
+  .envelope h2 {
+    font-size: 2.4rem;
+    color: var(--color-5);
+    margin: 1.6rem 3.2rem;
+  }
+
+  .action-group {
+    display: flex;
+    padding: 1.6rem;
+  }
+
+  fieldset {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border: none;
+  }
+
+  label {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-top: 1px solid var(--color-1);
+    padding: 1.6rem;
+  }
+</style>
