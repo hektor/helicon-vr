6bc1c870fe6df1eb3426d1b3fcd3906d32a52a7a
Hektor Misplon <hektor.misplon@pm.me>
2021-04-02 21:19:59 +0200

    Modularize

--- src/routes/world.route.svelte
+++ src/routes/world.route.svelte
@@ -11,93 +11,70 @@
 
   import Header from '../components/header.component.svelte'
   import TransportControls from '../components/transport-controls.svelte'
+  import MIDIDevices from '../components/midi-devices.component.svelte'
   import ToggleTheme from '../components/toggle-theme.component.svelte'
   import ChannelStrip from '../components/channel-strip.component.svelte'
   import AddTrack from '../components/add-track.component.svelte'
 
-  /*
-   * Menu
-   */
-
   let position = { x: 0, y: 0 }
   let shown = false
-  let trackMenu = null
+  let menuTrackId = null
 
-  /*
-   * Create channels
-   */
+  // Menu
+  const setMenuTrackId = id => (menuTrackId = id)
+  const showMenu = () => (shown = true)
+  const hideMenu = () => (shown = false)
+  const setMenuPosition = coordinates => (position = coordinates)
+  const closeMenu = () => (menuTrackId = null)
+  const isMenuClosed = ({ id }) => id !== menuTrackId
 
-  const master = new Channel($master$).toDestination()
-  let channels = $tracks$.map(({ volume, muted: mute, id }) =>
-    new Channel({ volume, mute, id }).connect(master),
-  )
+  // New track creation
+  const nextTrackId = () => $tracks$.length + 1
+  const nextTrackLabel = () => `Tracks ${nextTrackId()}`
+  const nextTrack = rest => ({ id: nextTrackId(), label: nextTrackLabel(), ...rest })
 
-  /*
-   * Synthesizer settings
-   */
+  const newChannel = ({ muted: mute, ...rest }) => new Channel({ mute, ...rest })
+  const setChannel = (channel, { muted: mute, volume }) => channel.set({ volume, mute })
 
+  const addChannel = channel => (channels = [...channels, channel])
+  const indexChannel = (channel, id) => channel.set({ name: id })
+  const removeChannel = ({ name: a }) => (channels = channels.filter(({ name: b }) => a !== b))
+
+  const addTrack = () => tracks$.next([...$tracks$, nextTrack({ volume: 0, muted: false })])
+  const indexTrack = (track, id) => ({ ...track, id: id + 1, label: `Track ${id + 1}` })
+  const removeTrack = () => tracks$.next($tracks$.filter(isMenuClosed).map(indexTrack))
+
+  const handleRemove = () => removeTrack() && closeMenu()
+
+  // Create from state
+  const master = newChannel($master$).toDestination()
+  let channels = $tracks$.map(track => newChannel(track).connect(master))
   const synth = new Synth($synth$).connect(channels[0])
 
-  /*
-   * Add a track
-   */
+  // Index all channels (master and track channels)
+  indexChannel(master, -1)
+  channels.forEach((channel, i) => indexChannel(channel, $tracks$[i].id))
 
-  const addTrack = () =>
-    tracks$.next([
-      ...$tracks$,
-      { id: $tracks$.length + 1, label: `Track ${$tracks$.length + 1}`, volume: 0, muted: false },
-    ])
+  const diff = (a, b) => a.filter(x => !b.includes(x))
+  const trackIds = () => $tracks$.map(({ id }) => id)
+  const channelIds = () => channels.map(({ name }) => name)
+  const removedTrackId = () => diff(channelIds(), trackIds())
+  const removedTrack = () => channels.filter(({ name }) => removedTrackId().includes(name))
+  const disposeRemovedTrack = () => removedTrack().forEach(channel => channel.dispose())
+  const removeRemovedTrack = () => removedTrack().forEach(channel => removeChannel(channel))
 
-  const indexTrack = (track, i) => ({
-    ...track,
-    id: i + 1,
-    label: `Track ${i + 1}`,
-  })
+  const updateChannels = () => channels.forEach((channel, i) => setChannel(channel, $tracks$[i]))
 
-  /*
-   * Remove track from its context menu,
-   * other tracks are reindexed and relabeled
-   */
+  const shouldAddTrack = () => $tracks$.length > channels.length
+  const shouldRemoveTrack = () => $tracks$.length < channels.length
 
-  const isMenuClosed = ({ id }) => id !== trackMenu
-  const removeTrack = () => {
-    tracks$.next($tracks$.filter(isMenuClosed).map(indexTrack))
-    trackMenu = null
-  }
-
-  const addChannel = channel => {
-    channels = [...channels, channel]
-  }
-  const indexChannels = () => channels.map((channel, i) => channel.set({ name: $tracks$[i].id }))
-  indexChannels()
-
-  /*
-   * Update master channel
-   */
-
+  // Handle state updates
   $: {
-    const { volume, muted: mute } = $master$
-    master.set({ volume, mute })
-  }
-
-  /*
-   * Updated track channels
-   */
-
-  $: {
-    if ($tracks$.length > channels.length)
-      addChannel(new Channel().set({ name: $tracks$.length }).connect(master))
-    if ($tracks$.length < channels.length) {
-      const trackIds = $tracks$.map(({ id }) => id)
-      const channelIds = channels.map(({ name }) => name)
-      const diff = channelIds.filter(x => !trackIds.includes(x))
-      // dispose unused channels
-      channels.filter(channel => diff.includes(channel.name) && channel.dispose())
-      // remove channel
-      channels = channels.filter(channel => !diff.includes(channel.name))
-    }
-
-    $tracks$.forEach(({ volume, muted }, i) => channels[i].set({ volume, mute: muted }))
+    master.set({ volume: $master$.volume, mute: $master$.muted })
+    if (shouldAddTrack()) addChannel(new Channel().set({ name: $tracks$.length }).connect(master))
+    if (shouldRemoveTrack()) disposeRemovedTrack()
+    if (shouldRemoveTrack()) removeRemovedTrack()
+    updateChannels()
   }
 
   /*
@@ -140,14 +117,10 @@
 
   Tone.Transport.scheduleRepeat(loop, '8n')
 
-  const showMenu = ({ clientX: x, clientY: y }, id) => {
-    trackMenu = id
-    shown = true
-    position = { x, y }
-  }
-
-  const hideMenu = () => {
-    shown = false
+  const handleContextMenu = ({ clientX: x, clientY: y }, id) => {
+    setMenuTrackId(id)
+    showMenu()
+    setMenuPosition({ x, y })
   }
 
   onDestroy(() => Transport.stop())
@@ -167,9 +140,7 @@
           bind:volume
           bind:muted
           selected={$selected$ === id}
-          on:contextmenu={e => {
-            showMenu(e, id)
-          }}
+          on:contextmenu={e => handleContextMenu(e, id)}
           on:click={selected$.set(id)}
           type="track"
         />
@@ -188,16 +159,16 @@
   <pre />
 </div>
 
-{#if trackMenu}
+{#if menuTrackId}
   <div
     id="context"
     style="--display: {shown ? 'block' : 'none'}; --top: {position.y}px; --left: {position.x}px"
   >
     <ul>
       <li>
-        <span>Track {trackMenu}</span>
+        <span>Track {menuTrackId}</span>
       </li>
-      <li class="option" on:click={removeTrack}><TrashCan24 />Remove</li>
+      <li class="option" on:click={handleRemove}><TrashCan24 />Remove</li>
     </ul>
   </div>
 {/if}
