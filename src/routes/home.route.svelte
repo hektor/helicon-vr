1a8bc950f55f25f29e08a20422dcaa155fe73767
Hektor Misplon <hektor.misplon@pm.me>
2021-04-02 14:32:13 +0200

    Separate browser support component

--- src/routes/home.route.svelte
+++ src/routes/home.route.svelte
@@ -1,15 +1,9 @@
 <script>
-  import CheckmarkFilled32 from 'carbon-icons-svelte/lib/CheckmarkFilled32'
   import LogoGithub32 from 'carbon-icons-svelte/lib/LogoGithub32'
-  import Misuse32 from 'carbon-icons-svelte/lib/Misuse32'
 
   import Header from '../components/header.component.svelte'
   import Typewriter from '../components/typewriter.component.svelte'
-
-  import { resolution } from '../stores/vr'
-
-  const supportsMIDI = !!navigator.requestMIDIAccess
-  const supportsVR = !!navigator.xr
+  import Supports from '../components/browser-support.component.svelte'
 </script>
 
 <div class="container">
@@ -41,48 +35,7 @@
           <a href="/#/faq">FAQ</a>
         </nav>
       </div>
-      <div class="supports-group">
-        <div class="supports-group-header">
-          <h2>Browser requirements</h2>
-          <a href="/#/requirements">Learn more</a>
-        </div>
-        <div class="supports">
-          <span>Resolution</span>
-          <select bind:value={$resolution} name="resolution">
-            <option selected={$resolution === 1} value={1}>Full</option>
-            <option selected={$resolution === 0.5} value={0.5}>Half</option>
-            <option selected={$resolution === 0.25} value={0.25}>Quarter</option>
-          </select>
-        </div>
-        <div class="supports">
-          {#if supportsMIDI}
-            <CheckmarkFilled32 />
-            <div>
-              <span>MIDI</span>
-              <small>Supported by your browser.</small>
-            </div>
-          {:else}
-            <Misuse32 />
-            <span>MIDI</span>
-            <small>Not supported by your browser.</small>
-          {/if}
-        </div>
-        <div class="supports">
-          {#if supportsVR}
-            <CheckmarkFilled32 />
-            <div>
-              <span>VR</span>
-              <small>Supported by your browser.</small>
-            </div>
-          {:else}
-            <Misuse32 />
-            <div>
-              <span>VR</span>
-              <small>Not supported by your browser.</small>
-            </div>
-          {/if}
-        </div>
-      </div>
+      <Supports />
     </div>
   </main>
 </div>
@@ -147,56 +100,5 @@
 
   .nav-group {
     flex: 1;
-  }
-
-  .supports-group {
-    display: flex;
-    flex-direction: column;
-  }
-
-  .supports-group-header {
-    display: flex;
-    align-items: center;
-    border-bottom: 1px solid var(--color-1);
-    padding: 1.6rem;
-  }
-
-  .supports-group-header > h2 {
-    margin-right: auto;
-    font-size: 2rem;
-  }
-
-  .supports-group-header > a {
-    font-size: 1.6rem;
-  }
-
-  .supports {
-    display: flex;
-    align-items: center;
-    padding: 1.6rem;
-    border-bottom: 1px solid var(--color-1);
-  }
-
-  .supports > div {
-    margin-left: 1.6rem;
-    display: flex;
-    flex-direction: column;
-  }
-
-  @media (max-width: 768px) {
-    .container {
-      flex-direction: column;
-    }
-
-    .col-left {
-      max-width: initial;
-    }
-  }
-
-  select {
-    flex: 1;
-    margin: 0.8rem;
-    margin-left: 3.2rem;
-    margin-right: 0;
   }
 </style>
