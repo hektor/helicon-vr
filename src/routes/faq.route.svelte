08196ee67874b6bdb7c990230d0804f50badec87
Hektor Misplon <hektor.misplon@pm.me>
2021-04-04 18:15:33 +0200

    Make summary styling global

--- src/routes/faq.route.svelte
+++ src/routes/faq.route.svelte
@@ -30,26 +30,6 @@
     flex-direction: column;
   }
 
-  summary {
-    padding: 3.2rem;
-    border-bottom: 1px solid var(--color-2);
-  }
-
-  details[open] summary ~ * {
-    animation: sweep 0.5s ease-in-out;
-  }
-
-  @keyframes sweep {
-    0% {
-      opacity: 0;
-      margin-left: -10px;
-    }
-    100% {
-      opacity: 1;
-      margin-left: 0px;
-    }
-  }
-
   p {
     padding: 3.2rem;
   }
