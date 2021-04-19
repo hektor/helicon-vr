2c2ec3a251611b68ba37815bf364fb717b59b6d6
Hektor Misplon <hektor.misplon@pm.me>
2021-04-19 20:13:14 +0200

    Refactor

--- src/components/sequencer.component.svelte
+++ src/components/sequencer.component.svelte
@@ -2,7 +2,7 @@
   import { InstancedFlow } from 'three/examples/jsm/modifiers/CurveModifier'
   import { TransformControls } from 'three/examples/jsm/controls/TransformControls'
   import {
-    BoxGeometry,
+    OctahedronGeometry,
     BufferGeometry,
     CatmullRomCurve3,
     Color,
@@ -26,6 +26,8 @@
     white: 0xffffff,
     gray1: 0x333333,
     gray2: 0x555555,
+    gray3: 0xaaaaaa,
+    white: 0xffffff,
   }
 
   const smallBoxDimensions = [0.5, 0.5, 0.5]
@@ -37,19 +39,19 @@
   const raycaster = new Raycaster()
 
   // Create geometry
-  const boxGeometry = new BoxGeometry(...smallBoxDimensions)
-  const boxMaterial = new MeshBasicMaterial({ color: colors.gray2 })
-  const boxActiveMaterial = new MeshBasicMaterial({ color: colors.white })
+  const geo = new OctahedronGeometry(0.5)
+  const mat = new MeshBasicMaterial({ color: colors.gray2 })
+  const matActive = new MeshBasicMaterial({ color: colors.gray3 })
 
   const curves = [
-    regularPolygon(3, 4, rad(-90)).map(point => ({ x: point[0], y: 2, z: point[1] })),
-    regularPolygon(4, 6, rad(-90)).map(point => ({ x: point[0], y: 4, z: point[1] })),
-    regularPolygon(5, 8, rad(-90)).map(point => ({ x: point[0], y: 6, z: point[1] })),
-    regularPolygon(16, 10, rad(-90)).map(point => ({ x: point[0], y: 8, z: point[1] })),
+    regularPolygon(3, 8, rad(-90)).map(point => ({ x: point[0], y: 2, z: point[1] })),
+    regularPolygon(4, 8, rad(-90)).map(point => ({ x: point[0], y: 10, z: point[1] })),
+    regularPolygon(5, 8, rad(-90)).map(point => ({ x: point[0], y: 18, z: point[1] })),
+    regularPolygon(16, 8, rad(-90)).map(point => ({ x: point[0], y: 26, z: point[1] })),
   ].map(curvePoints => {
     const curve = new CatmullRomCurve3(
       curvePoints.map(position => {
-        const handle = new Mesh(boxGeometry, boxMaterial)
+        const handle = new Mesh(geo, mat)
         handle.position.copy(position)
         curveHandles.push(handle)
         scene.add(handle)
@@ -70,7 +72,7 @@
     return { curve, line }
   })
 
-  flow = new InstancedFlow(curves.length, curves.length, boxGeometry, boxMaterial)
+  flow = new InstancedFlow(curves.length, curves.length, geo, mat)
 
   curves.forEach(({ curve }, i) => {
     flow.updateCurve(i, curve)
@@ -92,23 +94,37 @@
     control.object.position.clamp(new Vector3(-100, 1, -100), new Vector3(100, 10, 100))
   })
 
+  const renderActive = target => {
+    if (target) {
+      target.material = matActive
+      target.scale.set(1.5, 1.5, 1.5)
+    }
+  }
+
+  const renderInactive = target => {
+    if (target) {
+      target.material = mat
+      target.scale.set(1, 1, 1)
+    }
+  }
+
+  const setTarget = x => (target = x)
+
   $: {
     raycaster.setFromCamera(mouse, camera)
     const intersects = raycaster.intersectObjects(curveHandles)
     if (intersects.length) {
       if (target !== intersects[0].object) {
-        // Remove active material from current target
-        target && (target.material = boxMaterial)
-        // Set new target
-        target = intersects[0].object
+        renderInactive(target)
+        setTarget(intersects[0].object) // new target
       }
     } else {
-      target && (target.material = boxMaterial)
-      target = null
+      renderInactive(target)
+      setTarget(null)
     }
   }
 
-  $: target && (target.material = boxActiveMaterial)
+  $: renderActive(target)
 
   $: if (action) {
     action = false
