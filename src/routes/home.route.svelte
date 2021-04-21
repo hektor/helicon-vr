87d646fca656c12bf43bb20b085e3cba9eb34934
Hektor Misplon <hektor.misplon@gmail.com>
2021-04-21 17:46:51 +0200

    Use flex-grow hack

--- src/routes/home.route.svelte
+++ src/routes/home.route.svelte
@@ -28,14 +28,10 @@
       </div>
     </div>
     <div class="col col-right">
-      <div class="nav-group">
-        <nav>
-          <a href="/#/world">Discover</a>
-        </nav>
-        <nav>
-          <a href="/#/about">About</a>
-          <a href="/#/faq">FAQ</a>
-        </nav>
+      <div class="cta">
+        <a class="pulse" href="/#/world">Discover</a>
+        <a href="/#/about">Learn more</a>
+        <a href="/#/faq">FAQ</a>
       </div>
       <Supports />
     </div>
@@ -56,7 +52,8 @@
   }
 
   .col {
-    flex: 1;
+    /* flex grow hack*/
+    flex-grow: 99999;
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -64,10 +61,10 @@
   }
 
   .col-left {
-    color: var(--color-primary);
-    background: var(--color-1);
-    min-width: 32rem;
-    max-width: 64rem;
+    flex-grow: 1;
+    flex-basis: 64rem;
+    border-right: 1px solid var(--color-1);
+    min-width: 48rem;
   }
 
   .credits {
@@ -99,7 +96,50 @@
     justify-content: center;
   }
 
-  .nav-group {
+  .pulse {
+    background: var(--color-bg);
+    color: var(--color-primary);
+    box-shadow: 0 0 0 0 var(--color-primary);
+    animation: pulse 1.75s infinite cubic-bezier(0.8, 0, 0, 1);
+  }
+
+  .cta {
     flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    margin: 6.4rem;
+  }
+
+  .cta > a {
+    padding: 3.2rem;
+    display: flex;
+    min-width: 62%;
+    align-items: center;
+    font-size: 2.4rem;
+    letter-spacing: -0.05rem;
+    text-decoration: none;
+    border: 1px solid transparent;
+    transition: 0.16s cubic-bezier(0.8, 0.2, 0.2, 1);
+  }
+
+  .cta > a::after {
+    content: '';
+    position: relative;
+    flex: 1;
+    display: flex;
+    margin: 1.6rem;
+    border: 1px solid var(--color-2);
+  }
+
+  .cta > a:hover {
+    background: var(--color-1);
+    border: 1px solid var(--color-primary);
+  }
+
+  @keyframes pulse {
+    to {
+      box-shadow: 0 0 0 1.6rem var(--color-bg);
+    }
   }
 </style>
