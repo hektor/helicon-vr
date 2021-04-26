d68f23c09cd8f8f363b73c8f073907ba8e426cf5
Hektor Misplon <hektor.misplon@gmail.com>
2021-04-26 10:22:08 +0200

    Add 'first' array function

--- src/lib/array.js
+++ src/lib/array.js
@@ -1,9 +1,19 @@
 /**
  * Array difference (a - b)
- * 
- * @param {Array} a 
- * @param {Array} b 
- * @returns 
+ *
+ * @param {Array} a
+ * @param {Array} b
+ * @returns
  */
 
 export const diff = (a, b) => a.filter(x => !b.includes(x))
+
+/**
+ * Get first element of array
+ *
+ * @param {Array} arr
+ * @returns {any}
+ */
+
+export const first = arr => arr[0]
+
