d759bf85359a7025718638768356ae16b75897b2
Hektor Misplon <hektor.misplon@gmail.com>
2021-04-29 10:47:26 +0200

    Only emit if object has changed

--- src/stores/rhythms.js
+++ src/stores/rhythms.js
@@ -1,10 +1,16 @@
+import { distinctUntilChanged } from 'rxjs/operators'
 import { sequencer$ } from '../stores/euclid-sequencer'
 import { bjorklund } from '../lib/bjorklund'
 import { writable$ } from './utils/observable-store'
 
-const notes = ['C3', 'E3', 'G3', 'C4']
+const notes = ['C3', 'E3', 'G3', 'C4', 'G4']
 
-export const rhythms$ = writable$()
+// Hacky object comparison
+// Comparing JSON probably not way to go,
+// as we have to guarantee order
+const objEqual = (a, b) => JSON.stringify(a) === JSON.stringify(b)
+
+export const rhythms$ = writable$().pipe(distinctUntilChanged(objEqual))
 
 sequencer$.subscribe(sequencer => {
   rhythms$.next(
