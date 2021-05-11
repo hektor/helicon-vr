706a75d03cfe956e211d1ab9df9411b824f04b72
Hektor Misplon <hektor.misplon@gmail.com>
2021-05-11 19:43:17 +0200

    Add basic 'about' page contents

--- src/routes/about.route.svelte
+++ src/routes/about.route.svelte
@@ -11,6 +11,21 @@
     <h1>About</h1>
     <NavBack />
   </Header>
+  <main>
+    <h2>An immersive introduction to polyrhythm-based music production.</h2>
+    <p>
+      The source code for Helicon is available on
+      <a class="repo" href="https://www.github.com/hektormisplon/bap">GitHub</a>
+    </p>
+    <p>
+      Please check the <a href="/#/contact">FAQ</a> for any questions you might have, or
+      <a href="mailto:hektor.misplon@gmail.com?subject=Helicon">contact me</a>
+      if you cannot find what you are looking for.
+    </p>
+    <p>
+      Learn more about the <a href="/#/requirements">browser requirements</a> for using Helicon?
+    </p>
+  </main>
 </div>
 
 <style>
@@ -18,5 +33,20 @@
     flex: 1;
     display: flex;
     flex-direction: column;
+  }
+
+  main {
+    flex: 1;
+    max-width: 64rem;
+    margin: 6.4rem;
+  }
+
+  h2 {
+    padding: 1.6rem;
+    margin-top: 3.2rem;
+  }
+
+  p {
+    padding: 1.6rem;
   }
 </style>
